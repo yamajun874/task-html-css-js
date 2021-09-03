0cd3baca1170651599e8f9aae867cd8723b9e893
yamajun874 <j.yamayamada.874@gmail.com>
2021-09-03 14:54:19 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const target = document.getElementById("menu")
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav")
+  nav.classList.toggle('in');
+})
+
+
